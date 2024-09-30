9c85bbbcaa8eadb01ab2e40c745f33a41f676f62
nivetharajaa <120543388+nivetharajaa@users.noreply.github.com>
2024-09-30 16:08:55 +0530

    Update IMAGE TRANSFORMATION[4-6].py

--- IMAGE TRANSFORMATION[4-6].py
+++ IMAGE TRANSFORMATION[4-6].py
@@ -1,37 +1,44 @@
 
 ##iv)Image Reflection
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
-input_image=cv2.imread("got.jpg") 
-input_image=cv2.cvtColor(input_image,) 
-plt.axis("off") 
-plt.imshow(input_image)
+M_x = np.float32([[-1, 0, cols],
+                  [0, 1, 0],
+                  [0, 0, 1]])
+
+reflected_img_xaxis = cv2.warpAffine(input_img, M_x[:2], (cols, rows))
+
+plt.axis("off")
+plt.imshow(reflected_img_xaxis)
 plt.show()
-rows, cols, dim = 
-M_x=np.float
-reflected_img_xaxis=cv2.warpPerspective(input_image,M_x,(cols,rows))
-reflected_img_yaxis=cv2.warpPerspective(input_image,M_y,(cols,rows))
-plt.imshow(reflected_)
+
+M_y = np.float32([[1, 0, 0],
+                  [0, -1, rows],
+                  [0, 0, 1]])
+
+reflected_img_yaxis = cv2.warpAffine(input_img, M_y[:2], (cols, rows))
+
+plt.axis("off")
+plt.imshow(reflected_img_yaxis)
 plt.show()
 
 ##v)Image Rotation:
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
-input_image=cv2.imread("got.jpg") 
-input_image=cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
-angle=np.radians(45)
-M=np.float32([[np.cos(angle),-(np.sin(angle)),0],
-               [np.
-rotated_img=
+angle = np.radians(-60)
+M = np.float32([[np.cos(angle), -np.sin(angle), 0],
+                [np.sin(angle), np.cos(angle), 0]])
+center = (cols // 2, rows // 2)
+M = cv2.getRotationMatrix2D(center, -60, 1.0)
+rotated_img = cv2.warpAffine(input_img, M, (cols, rows))
+
+plt.axis('off')
+plt.imshow(rotated_img)
+plt.show()
 
 ##vi)Image Cropping:
 
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
-input_image=cv2.imread("got.jpg") 
-input_image=cv2.cvtColor(input_image,
+cropped_img = input_img[50:200, 200:400]
+plt.axis('off')
+plt.imshow(cropped_img)
 plt.show()
+
+
+
 
