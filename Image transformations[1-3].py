32e423d214542a6cfe862d540a8cb16367a853e9
nivetharajaa <120543388+nivetharajaa@users.noreply.github.com>
2024-09-30 16:12:32 +0530

    Update Image transformations[1-3].py

--- Image transformations[1-3].py
+++ Image transformations[1-3].py
@@ -3,41 +3,50 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-input_img=cv2.imread("color image of flower.jpg")
-input_img=cv2.cvtColor(input_img,cv2.)
+input_img = cv2.imread("rapunzel.jpg")
+# cv2.imshow("image webp",input_img)
+input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2RGB)
 plt.axis('off')
 plt.imshow(input_img)
 plt.show()
+
 rows,cols,dim=input_img.shape
-M=np.float32([[1,0,20],
-             [0,1,50],
-             [0,0,1]])
-translated_img=cv2.
+M=np.float32([[1,0,50],  [0,1,100],  [0,0,1]])
+translated_image=cv2.warpPerspective(input_img,M,(cols,rows))
 plt.axis('off')
-
-
+plt.imshow(translated_image)
+plt.show()
 
 ##ii)Image Scaling
 
-scaled_img=cv2.warpPerspective(input_img,M,(cols,rows))
+scale_factor = 1.5
+M_scale = np.float32([[scale_factor, 0, 0],
+                      [0, scale_factor, 0],
+                      [0, 0, 1]])
+
+scaled_img = cv2.warpAffine(input_img, M[:2], (int(cols*scale_factor), int(rows*scale_factor)))
 plt.axis('off')
 plt.imshow(scaled_img)
 plt.show()
 
 
 ##iii)Image Shearing
-M_x=np.float32([[1,0.2,0],
-               [0,1,0],
-               [0,0,1]])
-M_y=np.
-sheared_img_xaxis=cv2.warpPerspective(input_img,M_x,(cols,rows))
-sheared_img_yaxis=
+M_x = np.float32([[1, 0.2, 0],
+                  [0, 1, 0],
+                  [0, 0, 1]])
+
+sheared_img_xaxis = cv2.warpAffine(input_img, M_x[:2], (cols, rows))
 plt.axis('off')
 plt.imshow(sheared_img_xaxis)
 plt.show()
-plt.axis
-plt.show()
+M_y = np.float32([[1, 0, 0],
+                  [0.2, 1, 0],
+                  [0, 0, 1]])
 
+sheared_img_yaxis = cv2.warpAffine(input_img, M_y[:2], (cols, rows))
+plt.axis('off')
+plt.imshow(sheared_img_yaxis)
+plt.show()
 
 
 
